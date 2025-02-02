1b2ad16c9e61d11eb42933865337c95bc9fedc4f
JakubCzyzUE <61199748+JakubC0401@users.noreply.github.com>
2025-02-02 17:35:13 +0100

    Update app.js

--- Site Root/js/app.js
+++ Site Root/js/app.js
@@ -4,47 +4,14 @@
 
 
 */
+var a = 6;
 
-function add(x, y){
-    
-    return x + y;
+function test(){
+    alert(a);
+    a = 2;
+
 }
 
-function minus(x, y){
-    
-    return x - y;
-}
-function divide(x, y){
-
-    if(y != 0){
-
-        return x / y;
-    }
-    else{
-        return "nie dziel przez 0!"
-    }
-    
-}
-
-function poleTrojkata(wysokoscT, dlugoscPodstawy){
-    
-    poleT = (wysokoscT*dlugoscPodstawy)/2;
-
-    return poleT;
-}
-
-var a = add(10,2);
-
-var b = minus(10,8);
-
-var c = divide(10,5);
-
-var d = divide(10,0);
-
-var e = divide(7, 3);
+test();
 
 alert(a);
-alert(b);
-alert(c);
-alert(d);
-alert(e);
