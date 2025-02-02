61d6ef28d889d6df37b7eb9bc197886fe58731f5
JakubCzyzUE <61199748+JakubC0401@users.noreply.github.com>
2025-02-02 09:57:40 +0100

    update app.js

--- Site Root/js/app.js
+++ Site Root/js/app.js
@@ -1,7 +1,26 @@
-var x = 5;
-var y = 6;
 
-alert(x + y);
-//other comment
+var nazwaZmiennej; //variable - etykieta(nazwa zmiennej) <- deklaracja zmiennej
 
-/*przykładowy komentarz */
+nazwaZmiennej = 5; //przypisanie wartości
+
+var vat_pl = (1 + 23 * 1/100);
+
+var cenaNettoJava = 39;
+var cenaNettoAjax = 34;
+
+var cenaBruttoJava = cenaNettoJava * vat_pl;
+var cenaBruttoAjax = cenaNettoAjax * vat_pl;
+
+alert(cenaBruttoJava +" zł");
+alert(cenaBruttoAjax + " zł");
+
+/*
+typy zmiennych:
+
+1) liczby - number
+2) ciąg znaków - string
+3) wartości logiczne - true/false
+4) tablice,obiekty - ....
+5) null - pusta wartość
+6) undefined - niezdefiniowana
+*/
