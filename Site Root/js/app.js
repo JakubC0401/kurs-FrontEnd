c27a0f46960a06b5b3bd9db73f167f7fe5d06956
JakubCzyzUE <61199748+JakubC0401@users.noreply.github.com>
2025-02-02 12:56:44 +0100

    update app.js

--- Site Root/js/app.js
+++ Site Root/js/app.js
@@ -1,20 +1,20 @@
 
-/*OPERATORY LOGICZNE
+/*INSTRUKCJE WARUNKOWE
 
-!   -   negacja
-
-&&  -   koniunkcja "I" - W TYM SAMYM MOMENCIE SPEŁNIONE, jest 1 tylko wtedy gdy oba są prawdziwe
-
-
-||  -   alternatywa "LUB" - pierwsze lub drugie jest prawdziwe wtedy 1. Jest fałszywa tylko wtedy gdy oba są fałszywe.
 
 
 */
 
-var a = 3;
-var b = 6;
-
-if(a > b || a == 3)
-    alert("tak")
-else
-    alert("nie");
+var a = 5;
+var b = 7;
+if(a < b){
+    alert("a jest mniejsze od b");
+}
+else if(a > b){
+    alert("a jest większe od b");
+}
+else{
+    alert("a: "+ a);
+    alert("b: "+ b);
+    alert("coś innego");
+}
