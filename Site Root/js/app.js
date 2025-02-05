e60032bf49ec4755f58e740d711ec62ecd3665af
JakubCzyzUE <61199748+JakubC0401@users.noreply.github.com>
2025-02-05 13:53:27 +0100

    update app.js

--- Site Root/js/app.js
+++ Site Root/js/app.js
@@ -9,6 +9,7 @@ var myDiv = document.getElementById("test");
 
 myDiv.innerHTML = "nowa treść zmienona w JS";
 
+/*
 var personObject = {
     name:"John",
     surname:"Doe",
@@ -24,21 +25,23 @@ var personObject = {
     }
 
 };
-
-myDiv.innerHTML = personObject; //działa pod nadpisałem domyślny toString();
-/*
-var osoba = new Object({
-    name:"John",
-    surname:"Doe",
-    age: 46,
-    downloadInformation: function()
-    {
-        return this.name + " " + this.surname +" "+ this.age;
-    },
-
-    toString: function()
-    {
-        return this.name + " "+this.surname; //nadpisanie domyślnej metody toString()
-    }
-})
 */
+
+function Person(name, surname, age){  ///konstruktor klasy Person
+
+    this.name = name,
+    this.surname = surname,
+    this.age = age
+
+    this.toString = function(){
+       return this.name + " " + this.surname + " " + this.age;
+    };
+};
+
+var x = new Person("Adrian", "Kowalski", 45);
+var y = new Person("John","Doe",32);
+var z = new Person("Krystyna","Kamionka",65);
+
+myDiv.innerHTML = x + "<br>" + y + "<br>" + z; //działa bo nadpisałem domyślny toString();
+
+
