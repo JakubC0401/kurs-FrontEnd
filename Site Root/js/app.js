e563aed962a01ba46c5f173fce3a51ec5243032b
JakubCzyzUE <61199748+JakubC0401@users.noreply.github.com>
2025-02-07 16:03:08 +0100

    update app.js

--- Site Root/js/app.js
+++ Site Root/js/app.js
@@ -1,10 +1,4 @@
 
-/*
-
-*/
-
-//tablica w JS jest obiektem - ma swoje właściwości i funkcje.
-
 var tabProducts = [
     "PHP",
     "MySql",
@@ -15,41 +9,12 @@ var tabProducts = [
 ];
 
 
-/*
-var result = document.getElementById("result");
 
-var i = 0;
-while(i < tabProducts.length)
-{
-
-    result.innerHTML += tabProducts[i] + "<br>";
-    i++;
-};
-*/
-
-/*
-
-var i = 0;
-
-do
-{
-
-    alert(i)
-    i++;
-
-}while(i < tabProducts.length)
-
-*/
-
-var result = document.getElementById("result");
-
-var kursy_programowania = document.getElementById("kurs_programowania").getElementsByTagName("li");
-
-var i = 0;
-while(i < kursy_programowania.length){
+for(i=0; i < 5; i++){
 
     
-    result.innerHTML += kursy_programowania[i].innerHTML + "<br>";
-    i++;
+  document.getElementById("result").innerHTML += i + "<br>";
+
+
 };
 
