ec0c2e1071e87d16ec0ef26baa96e2ba236e9b4a
JakubCzyzUE <61199748+JakubC0401@users.noreply.github.com>
2025-02-10 16:14:26 +0100

    update style/index and app

--- Site Root/js/app.js
+++ Site Root/js/app.js
@@ -10,41 +10,22 @@
     removeEventListener - metoda do usuwania zdarzeń z elementów
 */
 
-function makeEvent(event, eventObj)
-{
-    var e = window.event || event;  
-    var srcElement = e.target || e.srcElement;
 
-
-    var tmp = document.getElementById('tmp');
-
-    tmp.innerHTML = "źródło eventu: "+ srcElement.tagName + "<br>event przypisany do tagu: "+ eventObj.tagName; 
-
-
-
-    
-};
 
 window.onload = function(){
 
-    var email = document.getElementById('email');
-    var submitFormButton = document.querySelector("#newsletter input[type='submit']");
-
-    submitFormButton.onclick = function(e){
-
-        var tmp = document.getElementById("tmp");
-        e.preventDefault();
+    var toTopButton = document.getElementById('toTopButton');
 
 
-        tmp.innerHTML = email.value;
-
-        if(email.value === 'jc@gmail.com'){
-            this.parentNode.submit();
+    window.onscroll = function(){
+        if(window.scrollY > 300){
+            toTopButton.style.display = 'block';
+        } else {
+            toTopButton.style.display = 'none';
         }
-        else{
-            alert("Niepoprawny email");
-        }
-
-        
     }
+
+    toTopButton.onclick = function(){
+        window.scrollTo(0, 0);
+    };
 };
