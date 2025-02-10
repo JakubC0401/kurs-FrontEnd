91c6f0d6b5ac2f30c6d08ca51984c9a86cf7d0c5
JakubCzyzUE <61199748+JakubC0401@users.noreply.github.com>
2025-02-10 14:30:48 +0100

    Update all

--- Site Root/js/app.js
+++ Site Root/js/app.js
@@ -10,66 +10,31 @@
     removeEventListener - metoda do usuwania zdarzeń z elementów
 */
 
-function createEvent(obj, eventName, functionToInvoke){
-    if(document.addEventListener){
-        obj.addEventListener(eventName, functionToInvoke);
-    }
-    else{
-        obj.attachEvent('on' + eventName, functionToInvoke);
-    }
+function makeEvent(event, str)
+{
+    var e = window.event || event;  
 
-};
+    var tmp = document.getElementById('temp');
 
-function removeEvent(obj, eventName, functionToInvoke){
-    if(document.removeEventListener){
-        obj.removeEventListener(eventName, functionToInvoke);
-    }
-    else{
-        obj.detachEvent('on' + eventName, functionToInvoke);
-    }
-};
+    tmp.innerHTML = e.clientX + ' ' + e.clientY + ' ' + str;   
 
-function changleColor(){
 
-    this.style.backgroundColor = 'red';
+    var tooltip = document.getElementById('tooltip');
 
-};
-function changleColor2(){
+    tooltip.style.display = 'block';
+
+    tooltip.style.left = e.clientX + 10 + 'px';
+    tooltip.style.top = e.clientY + 10 + 'px';
     
-    this.style.backgroundColor = 'green';
-
-};
-function fontSizeUp(){
-    var fontSize = parseInt(window.getComputedStyle(this).fontSize);
-
-    this.style.fontSize = ++fontSize + 'px';
 };
 
 window.onload = function(){
 
-    var test = document.getElementById('testDiv3');
+    var test = document.getElementById('testDiv');
 
-    var stop = document.getElementById('stop');
+    test.onmousemove = function(event){
 
-    createEvent(test, 'mouseover', changleColor);
-    createEvent(test, 'mouseover', fontSizeUp);
-    createEvent(test, 'mouseout', changleColor2);
-
-    removeEvent(stop, 'click', fontSizeUp);
-
-    //test.onmouseover = changleColor;
-    //test.onmouseout = changleColor2;
-
-    /*
-    test.addEventListener('mouseover', changleColor);
-    test.addEventListener('mouseover', fontSizeUp);
-    test.addEventListener('mouseout', changleColor2);
-
-
-    stop.addEventListener('click', function(){
         
-        test.removeEventListener('mouseover', fontSizeUp);
-      
-    });
-    */
+        makeEvent(event, this.tagName);
+    };
 };
