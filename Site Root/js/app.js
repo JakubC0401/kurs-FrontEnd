a7071230a50797967c5389e95059e197ff1e731a
JakubCzyzUE <61199748+JakubC0401@users.noreply.github.com>
2025-02-11 11:09:13 +0100

    add new image and folder images, update index, app and style.css

--- Site Root/js/app.js
+++ Site Root/js/app.js
@@ -11,9 +11,13 @@
 */
 
 
+function movingImage(e, objectMove){
+    objectMove.style.left = e.clientX - objectMove.width/2 + 'px';
+    objectMove.style.top = e.clientY - objectMove.height/2 + 'px';
+}
 
 window.onload = function(){
-
+    var wykrzyknik = document.getElementById('wykrzyknik');
     var toTopButton = document.getElementById('toTopButton');
 
 
@@ -28,4 +32,23 @@ window.onload = function(){
     toTopButton.onclick = function(){
         window.scrollTo(0, 0);
     };
+
+    wykrzyknik.onmousedown = function(){
+
+        var tempSelf = this;
+
+        document.onmousemove = function(e){
+
+            movingImage(e, tempSelf);
+
+        };
+    };
+
+    wykrzyknik.onmouseup = function(){
+        document.onmousemove = null;
+    };
+
+    wykrzyknik.ondragstart = function(e){
+        e.preventDefault();
+    }
 };
