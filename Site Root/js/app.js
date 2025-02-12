972bb2f97b15691459d335c79255b0b3748412bb
JakubCzyzUE <61199748+JakubC0401@users.noreply.github.com>
2025-02-12 10:54:54 +0100

    update index, adding jquerry

--- Site Root/js/app.js
+++ Site Root/js/app.js
@@ -1,35 +1,8 @@
 
-/*
 
-    Events - zdarzenia - to co się dzieje w przeglądarce
-
-
-    DHTML - Dynamic HTML - HTML + CSS + JS
-
-    addEvetListener - metoda do dodawania zdarzeń do elementów
-    removeEventListener - metoda do usuwania zdarzeń z elementów
-*/
 window.onload = function() {
-
-    var mainImageVar = document.getElementById('mainImage');
-
-    var image = new Image();
-
-    mainImageVar.appendChild(image);
-
-    var thumbnails = document.getElementsByClassName('thumbnail');
     
-    var currentthumbnail = thumbnails[0];
-    currentthumbnail.className += " current";
-    image.src = currentthumbnail.src;
+    alert("lallalaa");
+    
 
-    for (var i = 0; i < thumbnails.length; i++) {
-        thumbnails[i].onmouseover = function() {
-            currentthumbnail.className += " current";
-            currentthumbnail.className = currentthumbnail.className.replace(" current", "");
-            image.src = this.src;
-            
-            currentthumbnail = this;
-        };
-    }
-};
+}
