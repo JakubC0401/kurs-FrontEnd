bb236c67eb6d047ab22df838135af60b0a839c11
JakubCzyzUE <61199748+JakubC0401@users.noreply.github.com>
2025-02-12 14:13:40 +0100

    update on index/app/style

--- Site Root/js/app.js
+++ Site Root/js/app.js
@@ -2,5 +2,22 @@
 $(document).ready(function() {
 
 
+    $("#myForm").submit(function(e){
+        var searchValue = $("input[name='searchValue']").val();
+
+        if(searchValue){
+            window.location.href = "https://www.google.pl/search?q=" + searchValue;
+        }
+        else{
+            alert("Please enter a search value");
+        }
+        e.preventDefault();
+    })
+
+    $("#rangeTest").change(function(){
+        $("#rangeValue").attr("value", $(this).val());
+    })
+
+  
 });
 
