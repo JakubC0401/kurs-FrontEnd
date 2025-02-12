dd45ae5d6df327765c777ac8f2939067e283d6fa
JakubCzyzUE <61199748+JakubC0401@users.noreply.github.com>
2025-02-12 13:21:27 +0100

    clear commit

--- Site Root/js/app.js
+++ Site Root/js/app.js
@@ -1,18 +1,6 @@
 
 $(document).ready(function() {
 
-    //$("#simpleForm :text").css("width", 100);
-
-    $("simpleForm :text").css({"width": 100, "background-color": "yellow"});
-
-    $("#simpleForm :checkbox").click(
-        function () 
-        { 
-            
-            var x = $("#simpleForm :checked").length;
-            $("#info").text("You have selected " + x + " checkboxes");
-        });
-
 
 });
 
