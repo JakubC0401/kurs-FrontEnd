4f56e59a69fdfdddff94991388c1a464afb69f4e
JakubCzyzUE <61199748+JakubC0401@users.noreply.github.com>
2025-02-12 12:50:15 +0100

    update app.js

--- Site Root/js/app.js
+++ Site Root/js/app.js
@@ -1,14 +1,6 @@
 
 $(document).ready(function() {
 
-    //$("img[alt]").css("border", "2px solid gray"); //tylko te taki img które posiada atrybut alt
 
-    //$("img[src='images/PROJEKT BOMBA.png']").css("border", "2px solid red"); //tylko to zdjęcie które ma src=images/PROJEKT BOMBA.png
-
-    //$("img[src*='PROJEKT']").css("border", "2px solid green"); //tylko to zdjęcie które ma src zawierający PROJEKT
-
-    //$("img[src$='png']").css("border", "2px solid blue"); //tylko to zdjęcie które ma src kończący się na png
-
-    //$("img[src^='images/PROJEKT']").css("border", "2px solid yellow"); //tylko to zdjęcie które ma src zaczynający się od images
 });
 
