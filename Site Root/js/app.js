c519b920dd4d5373f42c6b44550038e7477fc96d
JakubCzyzUE <61199748+JakubC0401@users.noreply.github.com>
2025-02-12 13:04:37 +0100

    update app and index

--- Site Root/js/app.js
+++ Site Root/js/app.js
@@ -1,6 +1,18 @@
 
 $(document).ready(function() {
 
+    //$("#simpleForm :text").css("width", 100);
+
+    $("simpleForm :text").css({"width": 100, "background-color": "yellow"});
+
+    $("#simpleForm :checkbox").click(
+        function () 
+        { 
+            
+            var x = $("#simpleForm :checked").length;
+            $("#info").text("You have selected " + x + " checkboxes");
+        });
+
 
 });
 
