3dbe21439197c530641208279b195b637b412491
JakubCzyzUE <61199748+JakubC0401@users.noreply.github.com>
2025-02-12 11:42:23 +0100

    add jquery filles/ update on app.js index.html and style.css

--- Site Root/js/app.js
+++ Site Root/js/app.js
@@ -1,11 +1,19 @@
 
+$(document).ready(function() {
+    // Your code here
+
+    // $("div > b").css("background-color", "red"); //pobranie bezpośredni tag b w tagu div
+   // $("div.a, li.b").css("color", "blue"); //pobranie tagu div z klasą a oraz tagu li z klasą b
+
+   //$("#amazing + div").css("color", "red"); //pobranie tagu div po tagu z id amazing
+
+   //$("#amazing ~ div").css("color", "red"); //pobranie tagów div po tagu z id amazing
 
 
-$(document).ready(
-    function() 
-    {
+   $("div").click(function(){
+       $(this).css("font-weight", "bold");
+   });
 
-        alert("lallalaa");
-    }
-);
+
+});
 
