bf5d156e375928bd509abe92d33622803b9dbbc2
JakubCzyzUE <61199748+JakubC0401@users.noreply.github.com>
2025-02-15 16:23:31 +0100

    opcja z wykonaniem tego za pomocą JS

--- Site Root/js/app.js
+++ Site Root/js/app.js
@@ -1,4 +1,23 @@
 
+function dragStart(event) {
+
+
+    event.dataTransfer.setData('id', event.target.id);
+
+}
+function dragOver(event) {
+    
+    event.preventDefault();
+    
+}
+function drop(event) {
+        
+    event.preventDefault();
+    var id = event.dataTransfer.getData('id');
+    var element = document.getElementById(id);
+    event.target.appendChild(element);
+        
+}
 $(document).ready(function() {
 
 
