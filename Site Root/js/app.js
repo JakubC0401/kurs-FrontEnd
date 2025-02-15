7ec81899f62f9dc7f04d7df4d1fccf58b9444540
JakubCzyzUE <61199748+JakubC0401@users.noreply.github.com>
2025-02-15 15:21:29 +0100

    clear commit

--- Site Root/js/app.js
+++ Site Root/js/app.js
@@ -1,20 +1,6 @@
 
 $(document).ready(function() {
 
-    var value = $('#AG input[data-type="name"]').attr('data-type');
-    //or
-    var value2 = $('#AG input[data-type="email"]').data('required');
-    //or
-    var value3 = $('#BC input[data-type="phone"]').attr('type');
 
-    alert(value);
-    alert(value2);
-    alert(value3);
-
-
-    //bez jQuery
-
-    var value4 = document.getElementById('AG').getElementsByTagName('input')[0].getAttribute('data-type');
-    alert(value4);
 });
 
