d31f9c19647f7c5813e73c8a36f78fe339592acb
JakubCzyzUE <61199748+JakubC0401@users.noreply.github.com>
2025-02-16 14:32:23 +0100

    clear commit

--- Site Root/js/app.js
+++ Site Root/js/app.js
@@ -1,40 +1,7 @@
 
-// function dragStart(event) {
-
-
-//     event.dataTransfer.setData('id', event.target.id);
-
-// }
-// function dragOver(event) {
-    
-//     event.preventDefault();
-    
-// }
-// function drop(event) {
-        
-//     event.preventDefault();
-//     var id = event.dataTransfer.getData('id');
-//     var element = document.getElementById(id);
-//     event.target.appendChild(element);
-        
-// }
 $(document).ready(function() {
 
 
-    $('#wykrzyknik').dragstart(function(event) {
-        var a = event.dataTransfer.setData('id', $(this).attr('id'));
-
-
-    });
-
-    $('#dropZone').dragover(function(event) {
-        event.preventDefault();
-    }).drop(function(event) {
-        event.preventDefault();
-        var id = event.dataTransfer.getData('id');
-        var element = document.getElementById(id);
-        event.target.appendChild(element);
-    });
 
 });
 
