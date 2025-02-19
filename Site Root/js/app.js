37153e2ee53c2d0f4b88d037025976c846a6b12a
JakubCzyzUE <61199748+JakubC0401@users.noreply.github.com>
2025-02-19 15:55:12 +0100

    update index style and app

--- Site Root/js/app.js
+++ Site Root/js/app.js
@@ -1,36 +1,23 @@
 $(document).ready(function() {
-    var canvas_var = document.getElementById("canvas_element");
-    var context = canvas_var.getContext("2d");
 
-    $("#canvas_element").mouseover(function(event) {
-        var { x, y } = getMousePos(canvas_var, event);
-        $("#info").html("(" + x + "," + y + ")");
+    $(".header span").each(function(i) {
+        $(this).addClass("gh" + i);
+        $(this).data("index", i); // Przechowujemy indeks w atrybucie data
     });
 
-    var i = 0;
+    $(".hidden_text").each(function(i) {
+        $(this).addClass("gh" + i);
+    });
 
-    $("#canvas_element").click(function(event) {
-        var { x, y } = getMousePos(canvas_var, event);
-
-        if (i % 2 === 0) {
-            context.beginPath();
-            context.moveTo(x, y);
+    $(".header span").on("click", function() {
+        let index = $(this).data("index"); // Pobieramy indeks klikniętego span
+        $(".gh" + index + ".hidden_text").toggle(); // Tylko odpowiedni element się rozwija
+        
+        if ($(this).text() === "więcej info") {
+            $(this).text("schowaj");
         } else {
-            context.lineTo(x, y);
-            context.stroke();
+            $(this).text("więcej info");
         }
-
-        i++;
     });
 
-    function getMousePos(canvas, event) {
-        var rect = canvas.getBoundingClientRect(); // Pobiera dokładną pozycję i skalę canvasa
-        var scaleX = canvas.width / rect.width;   // Obsługuje skalowanie
-        var scaleY = canvas.height / rect.height;
-
-        return {
-            x: (event.clientX - rect.left) * scaleX, // Normalizuje do prawidłowej skali
-            y: (event.clientY - rect.top) * scaleY
-        };
-    }
 });
