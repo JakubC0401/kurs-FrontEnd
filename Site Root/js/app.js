1ec99d75f955962a26547adb5d105c64c3b1866d
JakubCzyzUE <61199748+JakubC0401@users.noreply.github.com>
2025-02-20 15:10:17 +0100

    update style, index and app

--- Site Root/js/app.js
+++ Site Root/js/app.js
@@ -1,13 +1,49 @@
 $(document).ready(function() {
 
-    $("#course_list tr td:nth-child(2)").addClass("cell_control");
+    var stos = new Array();
+
+   $(":header").each(function(i){
+
+        stos.push($(this)[0].nodeName);
+        $(this).html("<a href='#top' name='etykieta"+(i+1)+"'>" + $(this).html());
+   })
+
+   prepareMenu(stos);
+   $("body").prepend($("<a name='top'</a>"));
 
 
-    $("#course_list tr").addClass("pattern1");
-    $("#course_list tr:even").addClass("pattern2");
-
-    $("#course_list tr:not(#course_list tr:first-childe)").bind("click",function(event)
-    {
-        alert("aaa");
-    })
 });
+
+
+function prepareMenu(stos){
+
+    // tab = new Array();
+    // tab['nazwa'+1] = 6;
+
+    // window.ala = 5;
+    // alert(window.ala);
+    for(var i=0; i <stos.length; i++){
+        
+        window['item' + i] = new Array();
+        window['item' + i].push(stos[i]);
+        window['item' + i].wartosci = new Array();
+    }
+
+    createMenu(stos);
+}
+
+function createMenu(stos){
+
+    while(stos.length){
+        var searched = stos.pop().chatAt(1);
+
+        for (var i = stos.length-1; i>=0; i--){
+            if(parseInt((stos[i].charAt(1)) + 1) == searched) {
+                window['item'+i].wartosci.push(stos.length);
+                break;
+            }
+        }
+    }
+
+    alert(item0);
+}
