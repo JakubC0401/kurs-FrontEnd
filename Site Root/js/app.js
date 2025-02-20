e2729b3558cbc59cfb3a63c67eb667d5520dbff1
JakubCzyzUE <61199748+JakubC0401@users.noreply.github.com>
2025-02-20 15:27:48 +0100

    update style index and app

--- Site Root/js/app.js
+++ Site Root/js/app.js
@@ -1,8 +1,33 @@
 $(document).ready(function() {
+    $("#course_list tr td:nth-child(2)").addClass("cell_control");
 
-    $("#course_list tr td:nth-child(2)").addClass("cell_control")
+    $("#course_list tr").addClass("pattern1");
+    $("#course_list tr:even").addClass("pattern2");
 
+    var hint = $("<div class='box'>testowy tekst</div>").css({
+        "position": "absolute",
+        "display": "none",
+        "background": "#eee",
+        "padding": "5px",
+        "border": "1px solid #ccc"
+    });
 
-    $("#course_list tr").addClass("pattern1")
-    $("#course_list tr:even").addClass("pattern2")
+    $("body").append(hint);
+
+    $("#course_list tr:not(:first-child)").hover(function(event) {
+        hint.css({
+            "display": "block",
+            "top": event.pageY + 10 + "px",
+            "left": event.pageX + 10 + "px"
+        });
+    }, function() {
+        hint.css("display", "none");
+    });
+
+    $("#course_list tr:not(:first-child)").mousemove(function(event) {
+        hint.css({
+            "top": event.pageY + 10 + "px",
+            "left": event.pageX + 10 + "px"
+        });
+    });
 });
