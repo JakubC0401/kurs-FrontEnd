bc22c9a104f8f1a1f32b6d5122c9741f1e35fa5b
JakubCzyzUE <61199748+JakubC0401@users.noreply.github.com>
2025-02-20 12:09:57 +0100

    clear commit

--- Site Root/js/app.js
+++ Site Root/js/app.js
@@ -1,23 +1,5 @@
 $(document).ready(function() {
 
-    $(".header span").each(function(i) {
-        $(this).addClass("gh" + i);
-        $(this).data("index", i); // Przechowujemy indeks w atrybucie data
-    });
-
-    $(".hidden_text").each(function(i) {
-        $(this).addClass("gh" + i);
-    });
-
-    $(".header span").on("click", function() {
-        let index = $(this).data("index"); // Pobieramy indeks klikniętego span
-        $(".gh" + index + ".hidden_text").slideToggle(300); // Tylko odpowiedni element się rozwija
-        
-        if ($(this).text() === "więcej info") {
-            $(this).text("schowaj");
-        } else {
-            $(this).text("więcej info");
-        }
-    });
+    
 
 });
