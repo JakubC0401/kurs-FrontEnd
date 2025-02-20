0c700c3c13d067a904bf2f47e5079813acb67ebe
JakubCzyzUE <61199748+JakubC0401@users.noreply.github.com>
2025-02-20 12:26:43 +0100

    update style index and app

--- Site Root/js/app.js
+++ Site Root/js/app.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
 
-    
+    $("#course_list tr td:nth-child(2)").addClass("cell_control")
 
+
+    $("#course_list tr").addClass("pattern1")
+    $("#course_list tr:even").addClass("pattern2")
 });
