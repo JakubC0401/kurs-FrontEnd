715d426863668d1ef1645d23377e4f60898e5ae2
JakubCzyzUE <61199748+JakubC0401@users.noreply.github.com>
2025-02-20 13:02:43 +0100

    update on app.js

--- Site Root/js/app.js
+++ Site Root/js/app.js
@@ -1,8 +1,13 @@
 $(document).ready(function() {
 
-    $("#course_list tr td:nth-child(2)").addClass("cell_control")
+    $("#course_list tr td:nth-child(2)").addClass("cell_control");
 
 
-    $("#course_list tr").addClass("pattern1")
-    $("#course_list tr:even").addClass("pattern2")
+    $("#course_list tr").addClass("pattern1");
+    $("#course_list tr:even").addClass("pattern2");
+
+    $("#course_list tr:not(#course_list tr:first-childe)").bind("click",function(event)
+    {
+        alert("aaa");
+    })
 });
