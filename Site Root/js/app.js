a1130dee3d101a53822ae69e488616ce9394e07d
JakubCzyzUE <61199748+JakubC0401@users.noreply.github.com>
2025-02-21 12:22:00 +0100

    update app.js

--- Site Root/js/app.js
+++ Site Root/js/app.js
@@ -1,6 +1,8 @@
+var hint = $("<div class = 'box'> testowy tekst </div>");
+
 $(document).ready(function() {
 
-    var hint = $("<div class = 'box'> testowy tekst </div>");
+    
     hint.css("opacity",0.9);
     $("body").prepend(hint);
 
@@ -24,12 +26,40 @@ $(document).ready(function() {
     $(":button[value='Pokaż']").click(function() {
        
         hint.animate({
-            "left":0,
+            
             "width":350,
             "height":350,
             "opacity":0.9
-        },500,function(){
+        },500).animate({
+            "left":0
+        }, function(){
             hint.css("display","flex");
         });
     });
+    setInterval(boxAnimation, 2000);
+
 });
+
+function boxAnimation(){
+    hint.animate({
+        "left":10,
+
+    },1000)
+    .animate({
+        "width": 0,
+        "height": 0,
+        "opacity": 0
+    }, 500,
+    function(){
+        hint.css("display","none");
+    }).animate({
+            
+        "width":350,
+        "height":350,
+        "opacity":0.9
+    },500).animate({
+        "left":0
+    }, function(){
+        hint.css("display","flex");
+    });
+}
