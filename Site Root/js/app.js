920d6df3e9d677da9255b229ba95393e45d001be
JakubCzyzUE <61199748+JakubC0401@users.noreply.github.com>
2025-02-22 12:34:27 +0100

    some changes/ install jquery UI

--- Site Root/js/app.js
+++ Site Root/js/app.js
@@ -1,28 +1,6 @@
 $(document).ready(function() {
-    swapImage();
+
+
+
 });
 
-function swapImage() {
-    var firstImage = $(".images_swapper:first-child");
-    firstImage.addClass("current_image").css("opacity", 1);
-
-    swap();
-}
-
-function swap() {
-    var current = $(".current_image");
-    var next = current.next(".images_swapper");
-
-    if (next.length === 0) {
-        next = $(".images_swapper:first-child");
-    }
-
-    next.addClass("next_image").css("opacity", 0);
-
-    current.animate({"opacity": 0}, 2000, function() {
-        $(this).removeClass("current_image");
-        next.removeClass("next_image").addClass("current_image");
-    });
-
-    next.animate({"opacity": 1}, 2000, swap);
-}
