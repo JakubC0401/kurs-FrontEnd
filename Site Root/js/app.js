41bc83fbef7c9bc73c983787e50c56d540f6955e
JakubCzyzUE <61199748+JakubC0401@users.noreply.github.com>
2025-02-22 12:59:54 +0100

    update index and app

--- Site Root/js/app.js
+++ Site Root/js/app.js
@@ -1,6 +1,22 @@
 $(document).ready(function() {
 
+    $("#dragMe").draggable({
+        cursor: "pointer"
+    });
 
+    $(".ui-draggable").css("font-size",20);
 
+    $("#dragMe").draggable("option","axis", "x");
+    $("#dragMe").draggable("option","helper", "clone");
+    $("#dragMe").draggable("option","opacity", 0.4);
+    $( "#dragMe" ).draggable({
+        stop: function( event, ui ) {
+            //$("#dragMe").draggable("option","disabled",true);
+            $("#dragMe").offset(ui.offset);
+            $("#dragMe").position(ui.position);
+        }
+      });
+
+   // $("#dragMe").draggable("option","disabled",true);
 });
 
